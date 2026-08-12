d9b9a605d0ddbc06a882a83a015396e3b0c0f7c3
arnavvashishtha-beep <arnavvashishtha8@gmail.com>
2026-08-12 22:33:48 +0530

    add c language

---
+++ day3.code2.c
@@ -0,0 +1,15 @@
+//Write a program to swap two numbers without using a third variable.
+#include<stdio.h>
+int main(){
+int a , b ,c;
+printf("enter a number=");
+scanf("%d",&a);
+printf("a=%d\n",a);
+printf("enter a number=");
+scanf("%d",&b);
+printf("b=%d\n",b);
+//swapping numbers a and b
+printf("a=%d\n",b);
+printf("b=%d",a);
+return 0 ;
+}
